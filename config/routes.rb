8609339f543156c7416dc589e914994cf85b3a80
Merve Birsin <mervebr34@outlook.com>
2020-04-04 16:01:58 +0300

    Crud added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  #resources :gets, :posts
+  resources :users
+  resources :articles
   root 'pages#home'
   get 'about', to:'pages#about'
+ # resources :articles, only: [:show, :index, :create, :new]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+  
